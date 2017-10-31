b794d357075be7864fba1e77eff22423a88c4ad7
kaleshasheik <kalesha.shaik20@gmail.com>
2017-10-31 14:46:50 +0530

    Add files via upload

---
+++ searchfile.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
+)
+
+func main() () {
+    
+    var fileName string
+    fmt.Print("Enter the file name you want to search : ")
+    fmt.Scan(&fileName)
+
+ SearchFile(fileName)
+}
+
+func SearchFile(input string){
+
+    searchDir, _ := os.Getwd()
+    fileList := []string{}
+
+    fmt.Println("current directory is : ",searchDir)
+    fmt.Println("")
+
+   filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+        fileList = append(fileList, path)
+        return nil
+    })
+
+    for _, file := range fileList {
+
+    	if(strings.Contains(file,input)){
+        fmt.Println("Absolute path of file is :", file)
+    }
+ 
+    }
+
+}
